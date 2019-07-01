9dd0547d412559cf314bca4fa5f6bc9aabbffedc
Farhan Kathawala <farhank@stanford.edu>
2019-07-01 13:53:31 -0700

    Fix Black linter

--- test_responses.py
+++ test_responses.py
@@ -940,11 +940,11 @@ def test_cookies_from_headers():
 
 
 def test_cookie_attribute_handling():
-    expected_domains = ['.stackoverflow.com']
-    expected_paths = ['/']
-    cookie_text = 'prov=c83dc7dc-e0b9-xxxx-xxxx-xxxxxxxxxxxx; domain=.stackoverflow.com;'
-    cookie_text = cookie_text + ' expires=Fri, 01-Jan-2055 00:00:00 GMT; path=/; HttpOnly'
-    headers = {'set-cookie': cookie_text}
+    expected_domains = [".stackoverflow.com"]
+    expected_paths = ["/"]
+    cookie_text = "prov=c83dc7dc-e0b9-xxxx-xxxx-xxxxxxxxxxxx; domain=.stackoverflow.com;"
+    cookie_text = cookie_text + " expires=Fri, 01-Jan-2055 00:00:00 GMT; path=/; HttpOnly"
+    headers = {"set-cookie": cookie_text}
     cookiejar = responses._cookies_from_headers(headers)
     assert cookiejar.list_domains() == expected_domains
     assert cookiejar.list_paths() == expected_paths
